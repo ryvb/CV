0fd950be96b2c9e6ee96edeb4daa5d76bc08632f
ryvb <royvb@hotmail.com>
2025-05-17 13:14:34 +0000

    longer_timeout

--- CV-backend/playwright.config.ts
+++ CV-backend/playwright.config.ts
@@ -73,7 +73,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
      command: 'npm run dev',
-     //timeout: 120 * 1000,
+     timeout: 120 * 10000,
      url: 'http://localhost:3000',
      reuseExistingServer: !process.env.CI,
   },
