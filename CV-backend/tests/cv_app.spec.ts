cbf6b85c80445d4798fb4a8f7e200f9bd013be43
ryvb <royvb@hotmail.com>
2025-05-17 12:59:44 +0000

    playwright&action

--- CV-backend/tests/cv_app.spec.ts
+++ CV-backend/tests/cv_app.spec.ts
@@ -2,17 +2,17 @@ import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
     await page.goto('');
-})
+});
 
 test.describe('CV', () => {
     test('has title', async ({ page }) => {
         await expect(page.getByRole('heading', { name: 'Roy Voorbach' })).toBeVisible();
-    })
+    });
     
     test('details can be opened', async ( {page }) => {    
         await page.getByRole('row', { name: 'Werkervaring Details' }).getByRole('button', { name: 'Details '}).click();
-        await expect(page.getByText('NCCW & Bright Answers')).toBeVisible()
-    })
-})
+        await expect(page.getByText('NCCW & Bright Answers')).toBeVisible();
+    });
+});
 
 
